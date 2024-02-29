9c06bc5ff37ea1d50bab2a36eb67bd56a17b2280
CARLAQUIGU <quispecarla79@gmailcom>
2024-02-28 20:53:08 -0400

    Clase Miercoles

--- rest/rest_client.py
+++ rest/rest_client.py
@@ -18,6 +18,7 @@ post_response = requests.request(method="POST",
                                     url=ruta_post, 
                                     json=nuevo_estudiante)
 print(post_response.text)
+
 ruta_post = url + "agrega_estudiante"
 nuevo_estudiante = {
     "nombre": "Carla",
@@ -42,3 +43,22 @@ print(get_response.text)
 ruta_get = url + "total_estudiantes"
 get_response = requests.request(method="GET", url=ruta_get)
 print(get_response.text)
+
+#DELETE 
+ruta_eleminar = url + "estudiantes"
+eliminar_response = requests.request(method="DELETE", url=ruta_eleminar)
+print(eliminar_response.text)
+
+#actualizacion 
+ruta_actualizar = url + "estudiantes"
+estudiante_actualizado={
+    "id": 1,
+    "nombre" : "Juan",
+    "apellido" : "Perez",
+    "carrera" : "Ingenieria Agronomica",
+}
+put_response = requests.request(
+    method="PUT", url=ruta_actualizar,
+    json=estudiante_actualizado
+)
+print(put_response.text)
